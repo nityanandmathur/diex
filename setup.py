58cb49c9bcc3360032bb094f4ee719efe1bf9fa6
Nityanand Mathur <nityanandmathur@gmail.com>
2024-06-05 20:29:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 readme = open('README.md').read()
 
 setup(
-    name='diex',
+    name='dinoexplorer',
     packages=find_packages(),
     version='0.1.0',
     description='DINO Explorer: A tool to explore DINO embeddings.',
